453a9b4e47aa8f1112921ae0489932883c5426fd
juansarea <74296636+juansarea@users.noreply.github.com>
2025-09-19 22:02:35 +0700

    version

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const VERSION = { 
-    version: "© 2025 Turah Assistant v2.0"
+    version: "© 2025 Turah Assistant v2.1"
 };
 export const API_CONFIG = { 
     apiKey: "AIzaSyDHALOs_VpIGq7TdTPykGJClVe_QG4gC-o"

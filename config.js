2cf7efc076303a100961838973f0ec1500e34fa2
juansarea <74296636+juansarea@users.noreply.github.com>
2025-09-22 13:47:59 +0700

    update version

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const VERSION = { 
-    version: "© 2025 Turah Assistant v2.1.2"
+    version: "© 2025 Turah Assistant v2.1.3"
 };
 export const API_CONFIG = { 
     apiKey: "AIzaSyDHALOs_VpIGq7TdTPykGJClVe_QG4gC-o"
